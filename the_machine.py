05770db80f4f13391ed40ebdd2d5397ce14957af
Akrhiin <michaelpace.ma@protonmail.com>
2023-03-26 14:50:12 -0400

    03.26.2023 - switch to sklearn

--- the_machine.py
+++ the_machine.py
@@ -6,7 +6,7 @@
 import os
 import sys
 import numpy as np
-import cv2 as cv
+import sklearn as sk
 
 # numpy settings ----------------------------------------------
 np.set_printoptions(threshold=sys.maxsize)
